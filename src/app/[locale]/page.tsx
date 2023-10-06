2e8ec1769d4ab0c8c95f92d6e73714209ad6970b
Daniel Seijo <daniseijo.dev@gmail.com>
2023-10-06 13:20:30 +0200

    i18n home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,14 @@
+import { useTranslations } from 'next-intl'
 import { Carousel } from './components/Carousel/Carousel.component'
 import Title from './components/Title/Title.component'
 import { carouselImages } from './data'
 
 export default function Home() {
+  const t = useTranslations('home')
+
   return (
     <div>
-      <Title title="Bodas Alternativas" subtitle="Un nuevo concepto de boda para cambiarlo todo." />
+      <Title title={t('title')} subtitle={t('subtitle')} />
       <Carousel imagesList={carouselImages} className="mt-6" />
     </div>
   )
