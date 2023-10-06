6d1f8e36ce340e10afc60b6ff8f10f29caa8c666
Daniel Seijo <daniseijo.dev@gmail.com>
2023-10-06 13:12:39 +0200

    Remove static params

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,10 +26,6 @@ export const metadata: Metadata = {
 
 const locales = ['en', 'es']
 
-export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }))
-}
-
 export default function RootLayout({
   children,
   params: { locale },
